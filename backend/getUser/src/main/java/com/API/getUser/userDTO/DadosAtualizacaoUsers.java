c8e97d04ee72d5a99767d3b633bc0527cd46a474
RyanGustavoGoncalves <ryan_gustavo09@outlook.com>
2023-12-09 20:39:48 -0300

    new crud operations

--- backend/getUser/src/main/java/com/API/getUser/userDTO/DadosAtualizacaoUsers.java
+++
@@ -1,4 +0,0 @@
-package com.API.getUser.userDTO;
-
-public record DadosAtualizacaoUsers(Long id_User,String username, String password) {
-}
--- backend/getUser/src/main/java/com/API/getUser/userDTO/DadosListagemUsers.java
+++
@@ -1,9 +0,0 @@
-package com.API.getUser.userDTO;
-
-import com.API.getUser.users.Users;
-
-public record DadosListagemUsers(Long id_User, String username, String email, String password) {
-    public DadosListagemUsers(Users users){
-        this(users.getId_User(),users.getUsername(), users.getEmail(), users.getPassword());
-    }
-}
